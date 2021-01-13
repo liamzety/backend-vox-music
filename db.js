0d5483c97c4ff23b66359ff795769ca873c2f536
liamzety <58632095+liamzety@users.noreply.github.com>
2021-01-13 15:05:52 +0200

    m

--- db.js
+++ db.js
@@ -1,9 +1,17 @@
 const Pool = require('pg').Pool;
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'dragond1',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'vox'
+// })
+
 const pool = new Pool({
-    user: 'postgres',
-    password: 'dragond1',
-    host: 'localhost',
+    user: 'irjeucgnbzlcra',
+    password: 'd19966227b517dfa1e121f1976b997d6c88eed6adb64382af421c0ec40e8e3b2',
+    host: 'ec2-34-248-148-63.eu-west-1.compute.amazonaws.com',
     port: 5432,
     database: 'vox'
 })
