ceba5ccc6845518faad03f7d99f9db13b9e7daa6
liamzety <58632095+liamzety@users.noreply.github.com>
2021-01-13 15:10:45 +0200

    m

--- db.js
+++ db.js
@@ -9,11 +9,8 @@ const Pool = require('pg').Pool;
 // })
 
 const pool = new Pool({
-    user: 'irjeucgnbzlcra',
-    password: 'd19966227b517dfa1e121f1976b997d6c88eed6adb64382af421c0ec40e8e3b2',
-    host: 'ec2-34-248-148-63.eu-west-1.compute.amazonaws.com',
-    port: 5432,
-    database: 'vox'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 })
 
 module.exports = pool;
