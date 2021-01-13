ff9cb65f9dc1a23b2f97c9e91d733ae3867e7dde
liamzety <58632095+liamzety@users.noreply.github.com>
2021-01-13 15:48:15 +0200

    m

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(session({
     cookie: { secure: false }
 }))
 
-// const corsOptions = {
-//     origin: ['https://vox-music.netlify.app', 'http://localhost:3000'],
-//     credentials: true
-// };
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    credentials: true
+};
+app.use(cors(corsOptions));
 
 const addPlaylistRoutes = require('./api/playlist/playlist.route')
 const addSongRoutes = require('./api/song/song.route')
