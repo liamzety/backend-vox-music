ff9d5bf4b5b342bdc2b6b1f1b98e5ff4c69644de
liamzety <58632095+liamzety@users.noreply.github.com>
2021-01-14 10:49:19 +0200

    change postgres to work both remote and local

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const Pool = require('pg').Pool;
 // })
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL_DEV || process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL || process.env.DATABASE_URL_DEV,
     ssl: process.env.DATABASE_URL ? true : false
 })
 pool.connect();
